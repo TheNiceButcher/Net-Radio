8de0d2421066debdc4b86ffec1966be0337f2e5e
Harris HAMDANI <harris.hamdani@epita.fr>
2021-04-22 17:59:41 +0200

    Client gérant le TCP entre lui et le diffuseur

--- Client.c
+++ Client.c
@@ -9,9 +9,61 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
-
-#define SIZE_TYPE 4
+#include <pthread.h>
+#include "Client.h"
+Client  * client;
+void * multi_cast(void * args)
+{
+	int sock = 0;
+	int fd = open("/dev/pts/2",O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+	memcpy(&sock,args,sizeof(int));
+	char tampon[1000];
+	while(1){
+		int rec=recv(sock,tampon,1000,0);
+		tampon[rec]='\0';
+		write(fd,tampon,strlen(tampon));
+	}
+	return NULL;
+}
+void * tcp(void * args)
+{
+	struct sockaddr_in adress_sock;
+	adress_sock.sin_family = AF_INET;
+	adress_sock.sin_port = htons(atoi(client->port_tcp));
+	inet_aton(client->addr_diff,&adress_sock.sin_addr);
+	int sock = 0;
+	while(1)
+	{
+		sleep(10);
+		sock = socket(PF_INET,SOCK_STREAM,0);
+		int r = connect(sock,(struct sockaddr *)&adress_sock,sizeof(struct sockaddr_in));
+		if(r != -1)
+		{
+			char to_send[SIZE_MESS + 2];
+			char message[SIZE_MSG+1];
+			sprintf(message,"Coucou");
+			for(int i = strlen(message); i < SIZE_MSG;i++)
+			{
+				message[i] = '#';
+			}
+			message[SIZE_MSG] = '\0';
+			sprintf(to_send,"MESS %s %s\r\n",client->id,message);
+			char mess[5];
+			sleep(10);
+			send(sock,to_send,SIZE_MESS + 2,0);
+			int r = recv(sock,mess,SIZE_TYPE,0);
+			mess[r] = '\0';
+			printf("%s\n",mess);
+			close(sock);
+		}
+		else
+		{
+			perror("Connect TCP");
+			break;
+		}
+	}
+	return NULL;
+}
 int main(int argc, char const *argv[]) {
 	if (argc != 2)
 	{
@@ -21,76 +73,38 @@ int main(int argc, char const *argv[]) {
 	char * fichier = malloc(strlen(argv[1]));
 	strcpy(fichier,argv[1]);
 	fichier[strlen(argv[1])] = '\0';
-	int fd = open(fichier,O_RDONLY);
-	if(fd == -1)
+	client = create_client(fichier);
+	if(client == NULL)
 	{
-		perror("Fichier de configuration inexistant");
-		return 0;
+		write(STDERR_FILENO,"Fichier de configuration incorrect",35);
+		return EXIT_FAILURE;
 	}
-	char contenu_fich[55];
-	int a = read(fd,contenu_fich,55);
-	contenu_fich[a] = '\0';
-	int indexes[5] = { 0 };
-	int i = 0;
-	int j = 0;
-	char id[9], addr_multi[16], port_multi[5],addr_cli[16],port_tcp[5];
-	while (contenu_fich[i] != '\0')
-	{
-		while(contenu_fich[i] != '\n')
-		{
-			i++;
-		}
-		indexes[j] = i;
-		i++;
-		j++;
-	}
-	for (size_t i = 0; i < 5; i++) {
-		int prec = 0;
-		int act = indexes[i];
-		if(i != 0)
-		{
-			prec = indexes[i-1] + 1;
-		}
-		switch (i) {
-			case 0:
-				strncpy(id,contenu_fich + prec,act - prec);
-				id[act - prec] = '\0';
-				break;
-			case 1:
-				strncpy(addr_multi,contenu_fich + prec,act - prec);
-				addr_multi[act - prec] = '\0';
-				break;
-			case 2:
-				strncpy(port_multi,contenu_fich + prec,act - prec);
-				port_multi[act - prec] = '\0';
-				break;
-			case 3:
-				strncpy(addr_cli,contenu_fich + prec,act - prec);
-				addr_cli[act - prec] = '\0';
-				break;
-			case 4:
-				strncpy(port_tcp,contenu_fich + prec,act - prec);
-				port_tcp[act - prec] = '\0';
-				break;
-		}
-	}
-	int sock=socket(PF_INET,SOCK_DGRAM,0);
+	//Socket pour le multi-cast
+	int sock_multi=socket(PF_INET,SOCK_DGRAM,0);
 	int ok=1;
-	int r=setsockopt(sock,SOL_SOCKET,SO_REUSEPORT,&ok,sizeof(ok));
+	int r=setsockopt(sock_multi,SOL_SOCKET,SO_REUSEPORT,&ok,sizeof(ok));
+	if(r == -1)
+	{
+		perror("Erreur setsockopt");
+		exit(EXIT_FAILURE);
+	}
 	struct sockaddr_in address_sock;
 	address_sock.sin_family=AF_INET;
-	address_sock.sin_port=htons(atoi(port_multi));
+	address_sock.sin_port=htons(atoi(client->port_multi));
 	address_sock.sin_addr.s_addr=htonl(INADDR_ANY);
-	r=bind(sock,(struct sockaddr *)&address_sock,sizeof(struct sockaddr_in));
+	r=bind(sock_multi,(struct sockaddr *)&address_sock,sizeof(struct sockaddr_in));
 	struct ip_mreq mreq;
-	mreq.imr_multiaddr.s_addr=inet_addr(addr_multi);
+	mreq.imr_multiaddr.s_addr=inet_addr(client->addr_multi);
 	mreq.imr_interface.s_addr=htonl(INADDR_ANY);
-	r=setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
-	char tampon[1000];
-	while(1){
-		int rec=recv(sock,tampon,1000,0);
-		tampon[rec]='\0';
-		printf("Message recu : %s\n",tampon);
-	}
+	r=setsockopt(sock_multi,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
+
+	//Socket pour la connexion TCP
+	int sock_tcp = socket(PF_INET,SOCK_STREAM,0);
+	pthread_t t1,t2;
+	pthread_create(&t1,NULL,multi_cast,&sock_multi);
+	pthread_create(&t2,NULL,tcp,&sock_tcp);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	free(client);
 	return 0;
 }
---
+++ Client.h
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SIZE_TYPE 4
+#define SIZE_ID 8
+#define SIZE_IP 15
+#define SIZE_MSG 140
+#define SIZE_PORT 4
+#define SIZE_MESS (SIZE_TYPE + SIZE_ID + SIZE_MSG + 2)
+typedef struct Client_t{
+	char id[SIZE_ID + 1];
+	char addr_multi[SIZE_IP + 1];
+	char port_multi[SIZE_PORT + 1];
+	char addr_diff[SIZE_IP + 1];
+	char port_tcp[SIZE_PORT + 1];
+} Client;
+
+Client * create_client(char * filename)
+{
+	int fd = open(filename,O_RDONLY);
+	if(fd == -1)
+	{
+		return NULL;
+	}
+	char contenu_fich[55];
+	int a = read(fd,contenu_fich,55);
+	contenu_fich[a] = '\0';
+	int indexes[5] = { 0 };
+	int i = 0;
+	int j = 0;
+	char id[SIZE_ID + 1], addr_multi[SIZE_IP + 1], port_multi[SIZE_PORT + 1],addr_diff[SIZE_IP + 1],port_tcp[SIZE_PORT + 1];
+	while (contenu_fich[i] != '\0')
+	{
+		while(contenu_fich[i] != '\n')
+		{
+			i++;
+		}
+		indexes[j] = i;
+		i++;
+		j++;
+	}
+	for (size_t i = 0; i < 5; i++) {
+		int prec = 0;
+		int act = indexes[i];
+		if(i != 0)
+		{
+			prec = indexes[i-1] + 1;
+		}
+		int taille = act - prec;
+		switch (i) {
+			case 0:
+				if (taille > SIZE_ID)
+				{
+					return NULL;
+				}
+				strncpy(id,contenu_fich + prec,taille);
+				id[taille] = '\0';
+				break;
+			case 1:
+				if (taille > SIZE_IP)
+				{
+					return NULL;
+				}
+				strncpy(addr_multi,contenu_fich + prec,taille);
+				addr_multi[taille] = '\0';
+				break;
+			case 2:
+				if (taille > SIZE_PORT)
+				{
+					return NULL;
+				}
+				strncpy(port_multi,contenu_fich + prec,taille);
+				port_multi[taille] = '\0';
+				break;
+			case 3:
+				if (taille > SIZE_IP)
+				{
+					return NULL;
+				}
+				strncpy(addr_diff,contenu_fich + prec,taille);
+				addr_diff[taille] = '\0';
+				break;
+			case 4:
+				if (taille > SIZE_PORT)
+				{
+					return NULL;
+				}
+				strncpy(port_tcp,contenu_fich + prec,taille);
+				port_tcp[taille] = '\0';
+				break;
+		}
+	}
+	Client * client = malloc(sizeof(Client));
+	sprintf(client->id,"%s",id);
+	sprintf(client->addr_multi,"%s",addr_multi);
+	sprintf(client->addr_diff,"%s",addr_diff);
+	sprintf(client->port_multi,"%s",port_multi);
+	sprintf(client->port_tcp,"%s",port_tcp);
+	return client;
+}
