b0eeb64d4d464791dd5ac1f39d11027f9ab771b8
Harris HAMDANI <harris.hamdani@epita.fr>
2021-04-26 23:22:09 +0200

    Ajout fichier Entite

---
+++ Entite.java
@@ -0,0 +1,54 @@
+public interface Entite {
+	/**
+	Renvoie mot à la bonne longueur (lg_voulu), en le complémentant avec des '#'
+	Renvoie null si mot a plus de lg_voulu caracères
+	**/
+	public static String ajout_diese(String mot,int lg_voulu)
+	{
+		int lg_mot = mot.length();
+		if (lg_mot > lg_voulu)
+		{
+			return null;
+		}
+		String mot_complet = new String(mot);
+		for (int i = lg_mot; i < lg_voulu; i++)
+		{
+			mot_complet += "#";
+		}
+		return mot_complet;
+	}
+	/**
+	Renvoie mot à la bonne longueur, en ajoutant des "0" au début de celui-ci
+	**/
+	public static String ajout_zero(String mot,int lg_voulu)
+	{
+		int lg_mot = mot.length();
+		if (lg_mot > lg_voulu)
+		{
+			return null;
+		}
+		String mot_complet = new String(mot);
+		for (int i = lg_mot; i < lg_voulu; i++)
+		{
+			mot_complet = "0" +  mot_complet;
+		}
+		return mot_complet;
+	}
+	public static String convert_ip(String ip)
+	{
+		String[] m = ip.split("\\.");
+		System.out.println(m.length);
+		String new_ip = new String();
+		for (int i = 0; i < m.length;i++)
+		{
+			new_ip += ajout_zero(m[i],3);
+			if(i != 3)
+			{
+				new_ip += ".";
+			}
+			System.out.println(m[i]);
+		}
+		System.out.println(new_ip);
+		return new_ip;
+	}
+}
