9df06f5ff181578633c44b65609ced053e5516cc
Harris HAMDANI <harris.hamdani@epita.fr>
2021-05-21 22:51:16 +0200

    Ajout vrai messages diffuseur + prise en charge fichier de confi absent

--- Diffuseur.java
+++ Diffuseur.java
@@ -3,27 +3,40 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
 /**
  Permet la création d'un diffuseur
 **/
 public class Diffuseur {
 	public static void main(String[] args) {
 		List<String> f = new ArrayList<>();
-		for (int i = 0; i < 5;i++)
-		{
-			f.add("Salut #mon gars");
-			f.add("Pk0");
-			f.add("Pourqio");
-		}
+		f.add("Bienvenue sur ce diffuseur\nCes messages sont des noms d'anime ou de manga");
 		try
 		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("message-diff.txt")));
+			String anime = "";
+			while((anime = br.readLine()) != null)
+			{
+				f.add(anime);
+			}
+			br.close();
 			Un_Diffuseur diff = new Un_Diffuseur(args[0],f);
 			diff.lancer();
 		}
+		//Fichier de configuration fourni invalide
 		catch(BadConfigFileException e)
 		{
 			String error_msg = e.getMessage();
 			System.out.println(e);
+		}
+		//Absence du nom du fichier de configuration
+		catch (ArrayIndexOutOfBoundsException fich) {
+			System.out.println("Pour lancer le diffuseur, il faut le fichier de configuration en argument");
+		}
+		catch (FileNotFoundException misse) {
+			System.out.println("Fichier message-diff.txt introuvable à la racine");
 		}
 		catch(Exception e1)
 		{
