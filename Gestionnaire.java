8b7eb946f5dbdfbb6b214ce97c426b2e118032c5
Harris HAMDANI <harris.hamdani@epita.fr>
2021-05-22 22:36:14 +0200

    Gestion erreur port gestionnaire

--- Gestionnaire.java
+++ Gestionnaire.java
@@ -3,15 +3,20 @@ Permet la creation d'un gestionnaire
 **/
 public class Gestionnaire {
 	public static void main(String[] args) {
-		//Absence du port et / ou du nombre maximal de diffuseur -> affichage erreur + arret programme 
+		//Absence du port et / ou du nombre maximal de diffuseur -> affichage erreur + arret programme
 		if(args.length != 2)
 		{
 			System.out.println("Un gestionnaire doit être lancé avec 2 arguments: son port, et son nombre maximal de diffuseur");
 			return;
 		}
-		int port = Integer.parseInt(args[0]);
-		int nb_diff = Integer.parseInt(args[1]);
-		Un_Gestionnaire gest = new Un_Gestionnaire(port,nb_diff);
-		gest.lancer();
+		try {
+			int port = Integer.parseInt(args[0]);
+			int nb_diff = Integer.parseInt(args[1]);
+			Un_Gestionnaire gest = new Un_Gestionnaire(port,nb_diff);
+			gest.lancer();
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Erreur Gestionnaire " + e.getMessage());
+		}
 	}
 }
